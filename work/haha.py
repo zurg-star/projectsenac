1e9b82ebb4e5e03cfecb7825266e706cada2c71f
zurg-star <zurg@email>
2026-06-16 21:09:12 -0300

    class160626

--- work/haha.py
+++ work/haha.py
@@ -1,14 +1,25 @@
-# trabalho com a lista compras_online.csv...
+# Arquivo de clientes de e-comerce..
+
 
 arquivo_de_clientes = open (
-    file= "inclass/compras_online.csv", 
+    file= "projectsenac/work/compras_online.csv", 
     mode= "r",
     encoding= "utf-8"
 )
 
 registro_de_clientes = arquivo_de_clientes.readlines()
 arquivo_de_clientes.close()
-pesquisar = input("O que você Procura?: ")
+
+
+print("1. Nome")
+print("2. Idade")
+print("3. Endereço")
+print("4. Telefone")
+print("5. O que o Cliente comprou")
+pesquisar = input("O que você Procura?:")   
+
+
+
 
 for i in registro_de_clientes:
     valor = i.strip().split(",")
@@ -17,6 +28,15 @@ for i in registro_de_clientes:
     endereço= valor[2]
     telefone= valor[3]
     comprou= valor[4]    
+    
 
-    if (valor in (pesquisar)):
-        print(valor)
+    if pesquisar == 1:
+        print(nome)
+    if pesquisar == 2:
+        print(idade)
+    if pesquisar==3:
+        print(endereço)
+    if pesquisar==4:
+        print(telefone)
+    if pesquisar==5:
+        print(comprou)
